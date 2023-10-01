728f1ba4cf6a98e9544564af4e9023dc50ab8d42
Uzytkownik <uzytkownik@kursGIT.pl>
2023-10-01 14:08:15 +0200

    created detail page

--- basketBlog/blog/views.py
+++ basketBlog/blog/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import BlogPost
+from django.http import Http404
 
 def home(request):
     posts = BlogPost.objects.all() # metoda objects jest dziedziczona z klasy model, wyciągamy wszytskie obiekty z tabeli
     return render(request, 'home.html', {'posts': posts})
+
+
+def detail(request, post_id):
+    # try:
+    #     post = BlogPost.objects.get(id=post_id)
+    # except BlogPost.DoesNotExist:
+    #     raise Http404(f'No post with id {post_id} found')
+    post = get_object_or_404(BlogPost, id=post_id)
+    return render(request, 'detail.html', {'post': post})
