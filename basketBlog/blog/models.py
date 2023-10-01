4b2641fd57a4fbc2f7a5d5cb469c4d01a9fb7a45
Uzytkownik <uzytkownik@kursGIT.pl>
2023-10-01 12:09:05 +0200

    Wprowadzenie modelu BlogPost

--- basketBlog/blog/models.py
+++ basketBlog/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=250)
+    date = models.DateField()
+    text = models.TextField()
+
+    def __str__(self) -> str:
+        return f'{self.date} | {self.title}'
